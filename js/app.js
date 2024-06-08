ec51375973ffb544a35c03e09c64d3273d10104c
BegaliyevXushnud <xushnudpubgmn1@gmail.com>
2024-06-08 23:40:28 +0500

    added about

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
 let header = document.querySelector(".header");
 let toggleBtn = document.querySelector(".header__btn-hamburger");
 let headerList = document.querySelector(".header__list");
-
 window.addEventListener("scroll", () => {
   if (window.scrollY > 10) {
     header.classList.add("header__shrink");
@@ -14,3 +13,14 @@ window.addEventListener("scroll", () => {
 toggleBtn.addEventListener("click",()=>{
   headerList.classList.toggle("header__list-show");
 })
+
+
+let hrs = document.getElementById("hrs");
+let minut = document.getElementById("minut");
+let second = document.getElementById("second");
+setInterval(()=>{
+  let currenTime = new Date();
+hrs.innerHTML = currenTime.getHours();
+minut.innerHTML = currenTime.getMinutes();
+second.innerHTML = currenTime.getSeconds();
+},1000)
